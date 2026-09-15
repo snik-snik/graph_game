9f508b4052421ef75851d3a9a1e0ef72becb532d
Chingis Dyshenov <dyshenovch@gmail.com>
2026-09-15 12:50:26 +0300

    fix: base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
     setupFiles: ['./vitest.setup.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
   },
-  base: '/graph_game/'
+  base: './' // относительный путь вместо абсолютного
 });
